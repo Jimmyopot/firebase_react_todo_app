c44bb2a92ceaf22e6edf604d43b5264671a4d547
Jimmyopot <jimmyopot@gmail.com>
2021-04-20 16:03:15 +0200

    Added more security features

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,16 @@
 import firebase from "firebase";
+import dotenv from 'dotenv';
+ 
+dotenv.config()
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyC4aQ9nk5oXLvnPRZspqzXqaC79RB4KTEY",
+    apiKey: process.env.REACT_APP_API_KEY,
     authDomain: "react-todo-app-7641e.firebaseapp.com",
     projectId: "react-todo-app-7641e",
     storageBucket: "react-todo-app-7641e.appspot.com",
-    messagingSenderId: "256136408781",
-    appId: "1:256136408781:web:8683a239ba79da40ebc384",
-    measurementId: "G-4H42ZV0B52"  
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID,  
 });
 
 const db = firebaseApp.firestore();
